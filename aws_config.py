66c982d33daceb2ffbfffc090a8ab1d6ce6248e7
poketomcraft-22 <poketomcraft22@gmail.com>
2025-10-02 17:46:17 +0200

    Update aws_config.py

--- aws_config.py
+++ aws_config.py
@@ -1,1 +1,13 @@
+import boto3
+
+AWS_ACCESS_KEY_ID = "TON_ACCESS_KEY"
+AWS_SECRET_ACCESS_KEY = "TON_SECRET_KEY"
+AWS_REGION = "us-east-1"  # ← ici tu mets la région de tes buckets
+
+s3_client = boto3.client(
+    "s3",
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=AWS_REGION
+)
 
